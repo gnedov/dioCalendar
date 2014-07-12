a1a38c2f0c0ee7802037183d2c66551f0e030d3b
gnedov <andrey.gnedov@gmail.com>
2014-07-12 16:13:36 +0300

    test merge to preMaster + add simple changes to use Spring

--- Calendar/src/com/agn/clndr/Main.java
+++ Calendar/src/com/agn/clndr/Main.java
@@ -5,22 +5,37 @@ import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
 public class Main {
 
-    public static void  main(String[] arg){
+    public static void main(String[] arg) {
         UUID id = null;
         String title = "cccccc";
         String description = "descr bnbnb kkfkf otoot";
         List<String> attenders = new ArrayList<String>();
-            attenders.add("eeeee@mail.ff");
-            attenders.add("vvvvv@mail.ff");
-        GregorianCalendar timeStart = new GregorianCalendar(2014,07,2,16,22,34);
-        GregorianCalendar timeEnd = new GregorianCalendar(2014,07,2,23,11,11);
+        attenders.add("eeeee@mail.ff");
+        attenders.add("vvvvv@mail.ff");
+        GregorianCalendar timeStart = new GregorianCalendar(2014, 7, 2, 16, 22, 34);
+        GregorianCalendar timeEnd = new GregorianCalendar(2014, 7, 2, 23, 11, 11);
 
+        /* // This commented code works fine for creating event
         EventStore evStore = new EventStore();
         CalendarService clndrService = new CalendarService(evStore);
 
         clndrService.createEvent(id, title, description, attenders, timeStart, timeEnd);
         clndrService.printEvent(); // TO DO...
+        */
+
+        // extend the application with Spring framework
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        CalendarService service = (CalendarService) context.getBean("calendarService");
+
+        String[] reservedCalendarNames = {"eventTitle_1", "eventTitle_2"};
+
+        for (String name : reservedCalendarNames)
+            service.createEvent(id, name, name, attenders, timeStart, timeEnd);
     }
 }
