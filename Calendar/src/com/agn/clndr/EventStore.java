f0d3079349205ce0ffc065cc908b2964c56cfc25
Oleg Borkhalenko <borkhalenko@gmail.com>
2014-07-16 20:05:28 +0300

    Added functions addEvent, removeEvent and MultiMap store

--- Calendar/src/com/agn/clndr/EventStore.java
+++ Calendar/src/com/agn/clndr/EventStore.java
@@ -1,28 +1,54 @@
 package com.agn.clndr;
 
 import java.util.*;
+import org.apache.commons.collections4.MultiMap;
+import org.apache.commons.collections4.map.MultiValueMap;
+import org.joda.time.DateTime;
 
 public class EventStore {
     private Map allEvents;
-    private Map titleMap;
+    private MultiMap titleMap;
+    private MultiMap timeStartMap; //if we want to use search by event.timeEnd we need add MultiMap timeEndMap
 
     public EventStore() {
         this.allEvents = new HashMap<UUID, Event>();
-        this.titleMap = new HashMap<String, ArrayList<UUID>>();
+        this.titleMap = new MultiValueMap<String,UUID>();
+        this.timeStartMap = new MultiValueMap<String, UUID> ();
     }
 
+    //TODO need verify the parameters (class EventStoreVerifier?). Id and Event must be not null
+    //addEvent()->add()
     public void addEvent(UUID id, Event event) {
-        List<UUID> listId = new ArrayList<UUID>();
+        allEvents=new HashMap<UUID, Event>();
+        titleMap=new MultiValueMap<String,UUID>();
+        timeStartMap=new MultiValueMap<DateTime, UUID>();
+    }
 
-        allEvents.put(id, event);
-        String evTitle = event.getTitle();
-        if (!titleMap.containsKey(evTitle)) {
-            listId.add(event.getId());
-            titleMap.put(evTitle, listId);
-        } else {
-            listId = (List<UUID>) titleMap.get(evTitle);
-            listId.add(event.getId());
-        }
+    //This own function must be added to EventStorage interface
+    //because, perhaps, in feature, we will need to change the data store, and we will not
+    //need an UUID
+    public void addEvent(Event event){
+        UUID uuid=UUID.randomUUID();
+        this.addEvent(uuid, event);
+    }
+
+    public Event removeEvent(UUID uuid){
+        Event event=(Event) allEvents.get(uuid);
+        if (event==null)    //it's possible, that allEvents have a record with correct uuid and null event
+            return null;
+        return this.removeEvent(event);
+    }
+
+    public Event removeEvent(Event event){
+        UUID uuid=event.getId();
+        titleMap.removeMapping(event.getTitle(), uuid);
+        timeStartMap.removeMapping(event.getTimeStart(), uuid);
+        allEvents.remove(uuid);
+        return event;
+    }
+
+    public int size(){
+        return allEvents.size();
     }
 
     public Map getAllEvents() {
