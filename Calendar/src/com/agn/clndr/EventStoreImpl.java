16b84c0344753fa28ca9f6b1df9d9e94f13545b4
gnedov <andrey.gnedov@gmail.com>
2014-07-20 18:39:22 +0300

    changes acc. to[vtegza] comments +format code [Ctrl+Alt+L]

--- Calendar/src/com/agn/clndr/EventStoreImpl.java
+++ Calendar/src/com/agn/clndr/EventStoreImpl.java
@@ -1,54 +1,54 @@
 package com.agn.clndr;
 
 import java.util.*;
+
 import org.apache.commons.collections4.MultiMap;
 import org.apache.commons.collections4.map.MultiValueMap;
 import org.joda.time.DateTime;
 
 public class EventStoreImpl implements EventStore {
-    //local code review (vtegza): add generic types for maps @ 20.07.14
-    private Map allEvents;
-    private MultiMap titleMap;
-    private MultiMap timeStartMap; //if we want to use search by event.timeEnd we need add MultiMap timeEndMap
+    private Map<UUID, Event> allEvents;
+    private MultiMap<String, UUID> titleMap;
+    private MultiMap<String, UUID> timeStartMap; //if we want to use search by event.timeEnd we need add MultiMap timeEndMap
 
     public EventStoreImpl() {
-        this.allEvents = new HashMap<UUID, Event>();
-        this.titleMap = new MultiValueMap<String,UUID>();
-        this.timeStartMap = new MultiValueMap<String, UUID> ();
+        this.allEvents = new HashMap<>();
+        this.titleMap = new MultiValueMap<>();
+        this.timeStartMap = new MultiValueMap<>();
     }
 
     //TODO need verify the parameters (class EventStoreVerifier?). Id and Event must be not null
     //addEvent()->add()
     public void addEvent(UUID id, Event event) {
-        allEvents.put(id,event);
-        titleMap.put(event.getTitle(),id);
-        timeStartMap.put(event.getTimeStart(),id);
+        allEvents.put(id, event);
+        titleMap.put(event.getTitle(), id);
+        timeStartMap.put(event.getTimeStart().toString(), id);
     }
 
     //This own function must be added to EventStorage interface
     //because, perhaps, in feature, we will need to change the data store, and we will not
     //need an UUID
-    public void addEvent(Event event){
-        UUID uuid=event.getId();
-        if (uuid==null)
-            uuid=UUID.randomUUID();
+    public void addEvent(Event event) {
+        UUID uuid = event.getId();
+        if (uuid == null)
+            uuid = UUID.randomUUID();
         this.addEvent(uuid, event);
     }
 
-    public boolean removeEvent(UUID uuid){
-        Event event=(Event) allEvents.get(uuid);
+    public boolean removeEvent(UUID uuid) {
+        Event event = allEvents.get(uuid);
         return this.removeEvent(event);
     }
 
-    public boolean removeEvent(Event event){
-        UUID uuid=event.getId();
+    public boolean removeEvent(Event event) {
+        UUID uuid = event.getId();
         titleMap.removeMapping(event.getTitle(), uuid);
-        timeStartMap.removeMapping(event.getTimeStart(), uuid);
+        timeStartMap.removeMapping(event.getTimeStart().toString(), uuid);
         allEvents.remove(uuid);
         return true;
     }
 
-    public int size(){
+    public int size() {
         return allEvents.size();
     }
 
@@ -58,64 +58,64 @@ public class EventStoreImpl implements EventStore {
     }
 
     public Event findById(UUID id) {
-        return (Event) allEvents.get(id);
+        return allEvents.get(id);
     }
 
-    public Collection<Event> findAllByTitle(String title){
+    public Collection<Event> findAllByTitle(String title) {
         if (!titleMap.containsKey(title))
-            return new ArrayList<Event>(0);
+            return new ArrayList<>(0);
         //TODO This code is bad?
         //local code review (vtegza): yes, there are lots of casts @ 20.07.14
-        Collection<UUID> uuids=((MultiValueMap)titleMap).getCollection(title);
-        Collection<Event> events=new ArrayList<Event>(uuids.size());
-        for (UUID id:uuids){
-            events.add((Event)allEvents.get(id));
+        Collection<UUID> uuids = ((MultiValueMap) titleMap).getCollection(title);
+        Collection<Event> events = new ArrayList<>(uuids.size());
+        for (UUID id : uuids) {
+            events.add(allEvents.get(id));
         }
         return events;
     }
 
     //TODO Think about getting all Events by time period (findAllByTimePeriod() put into CalendarService)
-    public Collection<Event> findAllByDate(DateTime date){
-        Iterator<Map.Entry<DateTime,UUID>> iterator=((MultiValueMap)timeStartMap).iterator();
-        Collection<Event> events=new ArrayList<Event>();
+    public Collection<Event> findAllByDate(DateTime date) {
+        Iterator<Map.Entry<DateTime, UUID>> iterator = ((MultiValueMap) timeStartMap).iterator();
+        Collection<Event> events = new ArrayList<Event>();
         //local code review (vtegza): lot of complexity, use keySet() @ 20.07.14
-        while (iterator.hasNext()){
-            Map.Entry<DateTime,UUID> entry=iterator.next();
-            if ((entry.getKey()).getYear()==date.getYear() &&
-                    (entry.getKey()).getDayOfYear()==date.getDayOfYear()){
-                UUID id=entry.getValue();
-                events.add((Event)allEvents.get(id));
+        while (iterator.hasNext()) {
+            Map.Entry<DateTime, UUID> entry = iterator.next();
+            if ((entry.getKey()).getYear() == date.getYear() &&
+                    (entry.getKey()).getDayOfYear() == date.getDayOfYear()) {
+                UUID id = entry.getValue();
+                events.add(allEvents.get(id));
             }
         }
         return events;
     }
 
-    public Collection <Event> findAllByTimePeriod(DateTime start, DateTime end){
-        Iterator<Map.Entry<DateTime,UUID>> iterator=((MultiValueMap)timeStartMap).iterator();
-        Collection<Event> events=new ArrayList<Event>();
-        while (iterator.hasNext()){
-            Map.Entry<DateTime, UUID> entry=iterator.next();
-            if (entry.getKey().isAfter(start) && entry.getKey().isBefore(end)){
-                UUID id=entry.getValue();
-                events.add((Event)allEvents.get(id));
+    public Collection<Event> findAllByTimePeriod(DateTime start, DateTime end) {
+        Iterator<Map.Entry<DateTime, UUID>> iterator = ((MultiValueMap) timeStartMap).iterator();
+        Collection<Event> events = new ArrayList<>();
+        while (iterator.hasNext()) {
+            Map.Entry<DateTime, UUID> entry = iterator.next();
+            if (entry.getKey().isAfter(start) && entry.getKey().isBefore(end)) {
+                UUID id = entry.getValue();
+                events.add(allEvents.get(id));
             }
         }
         return events;
     }
 
-    public Event findNextByDate(DateTime time){
+    public Event findNextByDate(DateTime time) {
         //local code review (vtegza): shine of iterators, stick to keySet if you need @ 20.07.14
-        Iterator<Map.Entry<DateTime,UUID>> iterator=((MultiValueMap)timeStartMap).iterator();
-        Map.Entry<DateTime,UUID> good_entry=null;
-        while (iterator.hasNext()){
-            Map.Entry<DateTime,UUID> entry=iterator.next();
+        Iterator<Map.Entry<DateTime, UUID>> iterator = ((MultiValueMap) timeStartMap).iterator();
+        Map.Entry<DateTime, UUID> good_entry = null;
+        while (iterator.hasNext()) {
+            Map.Entry<DateTime, UUID> entry = iterator.next();
             if (time.isBefore(entry.getKey()))
-                if (good_entry==null || time.isBefore(good_entry.getKey()))
-                    good_entry=entry;
+                if (good_entry == null || time.isBefore(good_entry.getKey()))
+                    good_entry = entry;
         }
-        if (good_entry==null){
+        if (good_entry == null) {
             return null;
         }
-        return (Event) allEvents.get(good_entry.getValue());
+        return allEvents.get(good_entry.getValue());
     }
 }
