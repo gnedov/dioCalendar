2f6bbd06ff493db77dd95ecea331863c488d8629
gnedov <andrey.gnedov@gmail.com>
2014-12-29 13:21:04 +0200

    add Logging to Client: slf4j + logback jars, Maven dependency and logback.xml; +newest ArgsParser jar.

--- Calendar/Client/src/main/java/com/agn/clndr/client/MainClient.java
+++ Calendar/Client/src/main/java/com/agn/clndr/client/MainClient.java
@@ -2,10 +2,13 @@ package com.agn.clndr.client;
 
 import com.agn.clndrclient.CalendarClient;
 import com.agn.cmdparser.ConsoleInput;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainClient {
+    private static final Logger LOG = LoggerFactory.getLogger(MainClient.class);
     public static void main(String[] args) {
-
+        LOG.info("Application started");
         CalendarClient calendarClient = new CalendarClientImpl();
         ConsoleInput consoleInput = new ConsoleInput(calendarClient);
 
