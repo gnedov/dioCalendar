b618b72ae6fafd84283db387a36719df16c54ea9
gnedov <andrey.gnedov@gmail.com>
2015-01-02 22:59:37 +0200

    added: logback.xml, Maven dependency, the first Logger in Main.

--- Calendar/Service/src/main/java/com/agn/clndr/service/Main.java
+++ Calendar/Service/src/main/java/com/agn/clndr/service/Main.java
@@ -1,6 +1,8 @@
 package com.agn.clndr.service;
 
 import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -8,7 +10,9 @@ import java.rmi.RemoteException;
 
 public class Main {
 
-    public static void main(String[] arg) {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        LOG.info("Application started");
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         CalendarService service = (CalendarService) context.getBean("calendarServiceServer");
 
